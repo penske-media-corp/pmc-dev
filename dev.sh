f2c46f7ab09f54d17dbd4c2ba4b157b4910d5eda
brandoncamenisch <brandoncamenisch@gmail.com>
2019-02-28 11:25:25 -0800

    WI-969: updates dev.sh startup script and sorts docker-compose yml file

--- dev.sh
+++ dev.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
 # @see: https://github.com/FiloSottile/mkcert
-# @NOTE: This command should be ran on the host before starting the stack.
-# @TODO: Need setup instructions for mkcert on local
 install_mkcert() {
 	mkcert -install
 	mkcert "${1}"
 }
 
-# @NOTE: to setup and start the container run
+# @NOTE: run this to setup and start the container for traefik
 # `source dev.sh && start_traefik`
-# @TODO: Make startup configurable for multiple environments
 start_traefik() {
-
-	if [ -z "${PMC_TRAEFIK_IP}" ]
-		then export PMC_TRAEFIK_IP='172.30.80.80'
+	if [ -z "${PMC_TRAEFIK_DOMAIN}" ]
+		then export PMC_TRAEFIK_DOMAIN="pmcdev.local"
 	fi
 
 	if [ -z "${PMC_TRAEFIK_GATEWAY}" ]
 		then export PMC_TRAEFIK_GATEWAY='172.30.0.1'
 	fi
 
-	if [ -z "${PMC_TRAEFIK_SUBNET}" ]
-		then export PMC_TRAEFIK_GATEWAY='172.30.0.0/16'
+	if [ -z "${PMC_TRAEFIK_IP}" ]
+		then export PMC_TRAEFIK_IP='172.30.80.80'
 	fi
 
 	if [ -z "${PMC_TRAEFIK_NETWORK}" ]
 		then export PMC_TRAEFIK_NETWORK='traefik'
 	fi
 
+	if [ -z "${PMC_TRAEFIK_SUBNET}" ]
+		then export PMC_TRAEFIK_GATEWAY='172.30.0.0/16'
+	fi
 
-	if [[ ! -f "_wildcard.pmcdev.local.pem" && ! -f "_wildcard.pmcdev.local-key.pem" ]]
-		then install_mkcert '*.pmcdev.local'
+	if [[ ! -f "_wildcard.${PMC_TRAEFIK_DOMAIN}.pem" && ! -f "_wildcard.${PMC_TRAEFIK_DOMAIN}-key.pem" ]]
+		then install_mkcert "*.${PMC_TRAEFIK_DOMAIN}"
 	fi
 
 	if [ -z "$(docker network list | grep traefik )" ]
