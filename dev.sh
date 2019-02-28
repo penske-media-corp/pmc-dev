71ce60cbdb781eea562a08ad631ebe573fa66b8a
brandoncamenisch <brandoncamenisch@gmail.com>
2019-02-28 11:09:47 -0800

    WI-969: updates dev.sh startup script and sorts docker-compose yml file

--- dev.sh
+++ dev.sh
@@ -4,17 +4,29 @@
 # @NOTE: This command should be ran on the host before starting the stack.
 # @TODO: Need setup instructions for mkcert on local
 install_mkcert() {
-	local domain="${1}"
 	mkcert -install
-	mkcert "${domain}"
+	mkcert "${1}"
 }
 
 # @NOTE: to setup and start the container run
 # `source dev.sh && start_traefik`
 # @TODO: Make startup configurable for multiple environments
 start_traefik() {
+
 	if [ -z "${PMC_TRAEFIK_IP}" ]
 		then export PMC_TRAEFIK_IP='172.30.80.80'
+	fi
+
+	if [ -z "${PMC_TRAEFIK_GATEWAY}" ]
+		then export PMC_TRAEFIK_GATEWAY='172.30.0.1'
+	fi
+
+	if [ -z "${PMC_TRAEFIK_SUBNET}" ]
+		then export PMC_TRAEFIK_GATEWAY='172.30.0.0/16'
+	fi
+
+	if [ -z "${PMC_TRAEFIK_NETWORK}" ]
+		then export PMC_TRAEFIK_NETWORK='traefik'
 	fi
 
 
@@ -23,10 +35,10 @@ start_traefik() {
 	fi
 
 	if [ -z "$(docker network list | grep traefik )" ]
-		then docker network create traefik --gateway 172.30.0.1 --subnet 172.30.0.0/16
+		then docker network create traefik --gateway $PMC_TRAEFIK_GATEWAY --subnet $PMC_TRAEFIK_SUBNET
 	fi
 
-	if [ -z "$(docker ps --filter "name=traefik" | grep traefik )" ]
+	if [ -z "$(docker ps --filter "name=${PMC_TRAEFIK_NETWORK}" | grep "${PMC_TRAEFIK_NETWORK}" )" ]
 		then docker-compose up -d
 	fi
 }
